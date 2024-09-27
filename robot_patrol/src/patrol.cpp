f0b1d74070d385de372e9f45038b0fa9971c0177
Eldgar <walikanani@gmail.com>
2024-09-27 03:54:45 +0000

    corrected type mistmatch for direction_ calc

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -1,7 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "geometry_msgs/msg/twist.hpp"
-#include <cmath>
+#define M_PI 3.14159265358979323846
 class Patrol : public rclcpp::Node
 {
 public:
@@ -46,7 +46,9 @@ private:
                 }
             }
             RCLCPP_INFO(this->get_logger(), "Index:  %d", safest_idx);
-            direction_ = (safest_idx - (msg->ranges.size() / 2)) * M_PI / (msg->ranges.size() / 2);
+            direction_ = (static_cast<double>(safest_idx) - (static_cast<double>(msg->ranges.size()) / 2.0)) * M_PI / (static_cast<double>(msg->ranges.size()) / 2.0);
+
+            RCLCPP_INFO(this->get_logger(), "Index Difference:  %d", (safest_idx - 330));
             RCLCPP_INFO(this->get_logger(), "Direction:  %f", direction_);
         }
         else
@@ -61,13 +63,34 @@ private:
 
         twist_msg.linear.x = 0.1;
 
-        if (direction_ != 0.0)
+        // Inside your control loop or appropriate function
+        if (direction_ > 2.0 || direction_ < -2.0)
         {
-            twist_msg.angular.z = direction_;
+            // Log the error if direction is out of range
+            RCLCPP_ERROR(this->get_logger(), "Direction value %f is out of bounds! It should be between -2 and 2.", direction_);
+
+            // Optionally, you can stop the robot or take corrective action
+            twist_msg.angular.z = 0.0;  // Set angular velocity to 0 to stop turning
+            twist_msg.linear.x = 0.0;   // Optionally stop linear motion as well
+
+            // Publish the twist message to stop the robot
+            velocity_publisher_->publish(twist_msg);
+
+            return;  // Return or handle the situation as needed
         }
         else
         {
-            twist_msg.angular.z = 0.0;
+            // If direction is within bounds, proceed with normal motion
+            if (direction_ != 0.0)
+            {
+                twist_msg.angular.z = direction_ / 2.0;
+            }
+            else
+            {
+                twist_msg.angular.z = 0.0;
+            }
+
+            velocity_publisher_->publish(twist_msg);
         }
 
         velocity_publisher_->publish(twist_msg);
